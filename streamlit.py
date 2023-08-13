55daff3b7d9f82dec69e541fa34cf4cbf557e6dc
Tom <90728533+tomlupo@users.noreply.github.com>
2023-08-13 15:50:20 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -9,40 +9,41 @@ def convex_glide_path(t, T, steepness):
     return 100 * np.power(1 - t / T, steepness)
 
 def concave_glide_path(t, T, steepness):
-    return 100 * np.power(t / T, steepness)
+    return 100 - 100 * np.power(t / T, steepness)
 
 def s_shape_glide_path(t, T, s_steepness):
-    return 100 / (1 + np.exp(s_steepness * (T / 2 - t)))
+    return 100 - 100 / (1 + np.exp(-s_steepness * (t - T / 2)))
 
 def parabolic_glide_path(t, T, peak_time, parabolic_steepness):
-    a = -parabolic_steepness / (peak_time * (peak_time - T))
-    return a * (t - peak_time) ** 2
-
-def plot_glide_paths(T, steepness, peak_time, parabolic_steepness, s_steepness):
-    t = np.linspace(0, T, 500)
-    
-    plt.figure(figsize=(10, 6))
-    
-    plt.plot(t, linear_glide_path(t, T), label="Linear", color="red")
-    plt.plot(t, convex_glide_path(t, T, steepness), label="Convex", color="green")
-    plt.plot(t, concave_glide_path(t, T, steepness), label="Concave", color="blue")
-    plt.plot(t, s_shape_glide_path(t, T, s_steepness), label="S-Shape", color="magenta")
-    plt.plot(t, parabolic_glide_path(t, T, peak_time, parabolic_steepness), label="Parabolic", color="cyan")
-    
-    plt.title("Risk Glide Paths")
-    plt.xlabel("Time (t)")
-    plt.ylabel("Risk (%)")
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-    st.pyplot()
+    a = parabolic_steepness / (peak_time * (peak_time - T))
+    return 100 - a * (t - peak_time) ** 2
 
 st.title("Risk Glide Paths Visualization")
 
-T = st.slider("Total Time Period", 50, 200, 100)
-steepness = st.slider("Steepness for Convex and Concave Paths", 1.0, 5.0, 3.0)
-peak_time = st.slider("Peak Time for Parabolic Path", 10, T-10, 50)
-parabolic_steepness = st.slider("Steepness for Parabolic Path", 1.0, 5.0, 2.0)
-s_steepness = st.slider("Steepness for S-Shape Path", 0.01, 0.5, 0.1)
+# Create side-by-side columns
+left_column, right_column = st.beta_columns(2)
 
-plot_glide_paths(T, steepness, peak_time, parabolic_steepness, s_steepness)
+with left_column:
+    T = st.slider("Total Time Period", 50, 200, 100)
+    steepness = st.slider("Steepness for Convex and Concave Paths", 1.0, 5.0, 3.0)
+    peak_time = st.slider("Peak Time for Parabolic Path", 10, T-10, 50)
+    parabolic_steepness = st.slider("Steepness for Parabolic Path", 1.0, 5.0, 2.0)
+    s_steepness = st.slider("Steepness for S-Shape Path", 0.01, 0.5, 0.1)
+
+t = np.linspace(0, T, 500)
+fig, ax = plt.subplots(figsize=(10, 6))
+
+ax.plot(t, linear_glide_path(t, T), label="Linear", color="red")
+ax.plot(t, convex_glide_path(t, T, steepness), label="Convex", color="green")
+ax.plot(t, concave_glide_path(t, T, steepness), label="Concave", color="blue")
+ax.plot(t, s_shape_glide_path(t, T, s_steepness), label="S-Shape", color="magenta")
+ax.plot(t, parabolic_glide_path(t, T, peak_time, parabolic_steepness), label="Parabolic", color="cyan")
+
+ax.set_title("Risk Glide Paths")
+ax.set_xlabel("Time (t)")
+ax.set_ylabel("Risk (%)")
+ax.legend()
+ax.grid(True)
+
+with right_column:
+    st.pyplot(fig)
